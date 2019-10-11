654e3fc32eabd68acf4c32cb4aac2a5c9b2be01c
karlmarx <5042021062karlmarx@gmail.com>
2019-10-11 11:33:14 -0400

    added DTOs and DAO interface

---
+++ code/src/main/java/com/karlmarxindustries/herospotter/dao/LocationDaoImpl.java
@@ -0,0 +1,32 @@
+package com.karlmarxindustries.herospotter.dao;
+
+import com.karlmarxindustries.herospotter.dto.Location;
+
+import java.util.List;
+
+public class LocationDaoImpl implements LocationDao {
+    @Override
+    public Location getLocationById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return null;
+    }
+
+    @Override
+    public Location addLocation(Location location) {
+        return null;
+    }
+
+    @Override
+    public void updateLocation(Location location) {
+
+    }
+
+    @Override
+    public void deleteLocationById(Location location) {
+
+    }
+}

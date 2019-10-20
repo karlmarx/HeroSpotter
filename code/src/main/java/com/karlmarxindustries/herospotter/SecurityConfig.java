e35015b4f0ecd798369703d36c07a7d24b1fbf39
karlmarx <5042021062karlmarx@gmail.com>
2019-10-20 19:08:36 -0400

    changed config of spring security so it is now working again.

--- code/src/main/java/com/karlmarxindustries/herospotter/SecurityConfig.java
+++ code/src/main/java/com/karlmarxindustries/herospotter/SecurityConfig.java
@@ -1,50 +1,54 @@
-//package com.karlmarxindustries.herospotter;
+package com.karlmarxindustries.herospotter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+                .antMatchers("/css/**","/js/**", "/", "/index", "/fragments/**", "/locations", "/powers", "/supers", "/sightings", "/organizations").access("permitAll()")
+                .antMatchers(HttpMethod.GET, "/deleteOrganization").hasRole("ADMIN")
+                .antMatchers( "/editLocation", "/editPowers", "/editOrganization", "/editSighting", "/editSuper").hasRole("ADMIN").and().
+//                .antMatchers(HttpMethod.POST, "/**").permitAll().and().
+               // .requestCache().requestCache(new NullRequestCache()).and().
+                formLogin()
+                .loginPage("/login").failureUrl("/login-error").and().cors().and().
+                csrf().disable();
 //
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//
-//
+
+    }
 //    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/css/**","/js/**", "/", "/index", "/fragments/**", "/locations", "/powers", "/supers", "/sightings", "/organizations").access("permitAll()")
-//                .antMatchers( "/editLocation", "/editPowers", "/editOrganization", "/editSighting", "/editSuper").hasRole("ADMIN")
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").failureUrl("/login-error");
-////
-//
+//    public void configure(WebSecurity webSecurity) {
+//        webSecurity.ignoring().antMatchers(HttpMethod.POST, "/**");
 //    }
-////    @Override
-////    public void configure(WebSecurity webSecurity) {
-////        webSecurity.ignoring().antMatchers(HttpMethod.POST, "/**");
-////    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
+                .and()
+                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 //
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-//                .and()
-//                .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
-//                .and()
-//                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-////
-//}
+}
